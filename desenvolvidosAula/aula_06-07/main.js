6974b956706bac37c39ea18b03619bf5deeb01d0
Deivison Pasa <deivisonpasa@gmail.com>
2022-07-06 21:39:06 -0300

    Aula 06-07

--- desenvolvidosAula/aula_06-07/main.js
+++ desenvolvidosAula/aula_06-07/main.js
@@ -11,10 +11,12 @@ document.getElementById('btn-substitui-p-texto').onclick = function () {
     document.getElementById('p-texto').innerHTML = 'O texto do parágrafo <b>foi substituído</b>.'
 }
 
+
 // Exemplo 3
 document.getElementById('btn-ocultar-exemplo-3').onclick = function () {
     document.getElementById('exemplo-3').style.display = 'none'
 }
+
 
 // Exemplo 4
 document.getElementById('liga-lampada').onclick = function () {
@@ -24,5 +26,21 @@ document.getElementById('desliga-lampada').onclick = function () {
     document.querySelector('.lampada').src = 'images/lamp-off.png'
 }
 
+
 // Exemplo 5
 console.log('A área do círculo é: ' + Math.PI * Math.pow(3,2))
+
+
+// Exemplo 6
+const data = new Date()
+const dia = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+document.querySelector('.dia-semana').innerText = dia[data.getDay()]
+
+
+// Exemplo 7
+document.getElementById('converter-temperatura').onclick = function () {
+    const celsius = document.querySelector('input[type=number]').value
+    const farenheit = document.querySelector('input[type=text]')
+    const temperaturaConvertida = celsius * 1.8 + 32
+    farenheit.value = temperaturaConvertida
+}
